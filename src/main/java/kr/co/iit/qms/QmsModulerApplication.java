7de25309395ae8033568814d4107aeaf4fc3eb13
unnine <eonninej@gmail.com>
2025-03-25 15:49:59 +0900

    create project

---
+++ src/main/java/kr/co/iit/qms/QmsModulerApplication.java
@@ -0,0 +1,13 @@
+package kr.co.iit.qms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QmsModulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QmsModulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/iit/qms/QmsModulerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.iit.qms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QmsModulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
